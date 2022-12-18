1a08fba9cde4460299838ad0ddeb2858a65f8852
krishkansal <krishkansal7@gmail.com>
2022-12-18 19:16:56 +0530

    update

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const bcrypt=require("bcrypt");
 require("./DB/conn");
 const data=require("./model/signup")
 const app=express()
-const port=3000 || process.env.PORT
+const port=process.env.PORT || 3000
 
 
 app.use(express.json());
